e16fff26dd33a7831cc79eb2f4d507352899a8d2
HALt <102590462+shifty-d@users.noreply.github.com>
2023-09-26 07:09:47 +0300

    Update mtk_gui.cpp

--- mtk/libshims/gui/mtk_gui.cpp
+++ mtk/libshims/gui/mtk_gui.cpp
@@ -7,6 +7,10 @@ extern "C" {
 }
 
 extern "C" {
+    void _ZN7android13GraphicBufferC1Ejjij(uint32_t inWidth, uint32_t inHeight, int32_t inFormat, uint32_t inUsage);
+}
+
+extern "C" {
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t, uint32_t, const android::Rect&, void**);
 
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
